b410fe786c7620d84e242ce227626f1c8f2c6b67
Parvin Malekzadeh <45303081+pmalekzadeh@users.noreply.github.com>
2024-01-09 19:44:19 -0500

    Update AKFSR_tag.py

--- AKFSR_tag.py
+++ AKFSR_tag.py
@@ -1,6 +1,6 @@
 import AKFSR
 # To test the 1v2 environment
-from Multi_Agent_SR.AKFSR_simple1v2_initials import Mu, Sigma
+from AKFSR_initial import Mu, Sigma
 from numpy.linalg import inv
 import numpy as np
 import argparse
